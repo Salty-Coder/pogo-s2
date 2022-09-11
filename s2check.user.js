5fe696a41a242baedc9ab7f9cc3813581ef04d4e
Salty-Coder <59771980+Salty-Coder@users.noreply.github.com>
2022-09-11 17:44:34 +0100

    Copy data to clipboard

--- s2check.user.js
+++ s2check.user.js
@@ -345,6 +345,8 @@
 			if (typeof text != 'string') {
 				text = JSON.stringify(text);
 			}
+			
+			copyToClipboard(text)
 
 			if (typeof window.saveFile != 'undefined') {
 				window.saveFile(text, filename, 'application/json');
@@ -3850,3 +3852,58 @@
 		wrapper(plugin_info);
 	}
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+function copyToClipboard(string) {
+  let textarea;
+  let result;
+
+  try {
+    textarea = document.createElement('textarea');
+    textarea.setAttribute('readonly', true);
+    textarea.setAttribute('contenteditable', true);
+    textarea.style.position = 'fixed'; // prevent scroll from jumping to the bottom when focus is set.
+    textarea.value = string;
+
+    document.body.appendChild(textarea);
+
+    textarea.focus();
+    textarea.select();
+
+    const range = document.createRange();
+    range.selectNodeContents(textarea);
+
+    const sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+
+    textarea.setSelectionRange(0, textarea.value.length);
+    result = document.execCommand('copy');
+  } catch (err) {
+    console.error(err);
+    result = null;
+  } finally {
+    document.body.removeChild(textarea);
+  }
+
+  // manual copy fallback using prompt
+  if (!result) {
+    const isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+    const copyHotkey = isMac ? '⌘C' : 'CTRL+C';
+    result = prompt(`Press ${copyHotkey}`, string); // eslint-disable-line no-alert
+    if (!result) {
+      return false;
+    }
+  }
+  return true;
+}
